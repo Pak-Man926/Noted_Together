600eb15bdd64580a3267f5e23ee7844ec634da8f
Pak_Man926 <davidkamau926@gmail.com>
2025-04-24 16:09:56 +0300

    Auto commit

--- notedtogether_server/lib/server.dart
+++ notedtogether_server/lib/server.dart
@@ -19,7 +19,7 @@ void run(List<String> args) async {
     authenticationHandler: auth.authenticationHandler,
   );
 
-  auth.AuthConfig()
+  auth.AuthConfig.set(config)
 
   // If you are using any future calls, they need to be registered here.
   // pod.registerFutureCall(ExampleFutureCall(), 'exampleFutureCall');
