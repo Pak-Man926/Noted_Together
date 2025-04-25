b739f8ae85fa1e6d51ee1649cce724689a9a8f92
Pak_Man926 <davidkamau926@gmail.com>
2025-04-25 14:19:40 +0300

    Auto commit

--- notedtogether_flutter/lib/main.dart
+++ notedtogether_flutter/lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Serverpod Example'),
+      home: const MyHomePage(title: 'Noted Together'),
     );
   }
 }
