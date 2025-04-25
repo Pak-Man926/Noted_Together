944c5fa1150a93d7de2009906e188fad2904d012
Pak_Man926 <davidkamau926@gmail.com>
2025-04-25 18:16:32 +0300

    Auto commit

--- notedtogether_server/lib/src/endpoints/notes_endpoint.dart
+++ notedtogether_server/lib/src/endpoints/notes_endpoint.dart
@@ -1,3 +1,3 @@
 import "package:notedtogether_server/src/generated/protocol.dart";
 import "package:serverpod/serverpod.dart";
-import "package:serverpod_auth_"
+import "package:serverpod_auth_server/"
