61a2554176437df7d5c34e95376f57243d83fdeb
Pak_Man926 <davidkamau926@gmail.com>
2025-04-25 14:14:26 +0300

    Auto commit

--- notedtogether_flutter/lib/main.dart
+++ notedtogether_flutter/lib/main.dart
@@ -15,7 +15,7 @@ var client = Client(
    
   ..connectivityMonitor = FlutterConnectivityMonitor();
 
-  Final sessionManager = SessionManager(caller: client.modules.)
+  Final sessionManager = SessionManager(caller: client.modules.auth)
 
 void main() {
   runApp(const MyApp());
