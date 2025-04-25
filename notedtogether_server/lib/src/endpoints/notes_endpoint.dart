50e7672baf08e0fe4237ca2741c47cb4f5177443
Pak_Man926 <davidkamau926@gmail.com>
2025-04-25 18:23:49 +0300

    Auto commit

--- notedtogether_server/lib/src/endpoints/notes_endpoint.dart
+++ notedtogether_server/lib/src/endpoints/notes_endpoint.dart
@@ -20,8 +20,8 @@ class NotesEndpoint extends Endpoint
 
   Future<List<Note>> getAllNotes(Session session) async
   {
-    var notes = await Note.db.find;
-
-    
+    var notes = await Note.db.find(
+      
+    )
   }
 }
