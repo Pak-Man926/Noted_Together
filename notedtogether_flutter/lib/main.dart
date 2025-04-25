bec607afdc6d002c00b01cb77dd17d6996af5771
Pak_Man926 <davidkamau926@gmail.com>
2025-04-25 14:07:25 +0300

    Auto commit

--- notedtogether_flutter/lib/main.dart
+++ notedtogether_flutter/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:notedtogether_client/notedtogether_client.dart';
 import 'package:flutter/material.dart';
 import 'package:serverpod_flutter/serverpod_flutter.dart';
-import "package:serverpod_auth_shared/serverpod_auth_shared
+import "package:serverpod_auth_shared_f/serverpod_auth_shared
 
 // Sets up a singleton client object that can be used to talk to the server from
 // anywhere in our app. The client is generated from your server code.
