3e05a9a4347171656850b37842405328d3185bdc
Pak_Man926 <davidkamau926@gmail.com>
2025-04-29 16:46:57 +0300

    Auto commit

--- notedtogether_flutter/lib/loading_screen.dart
+++ notedtogether_flutter/lib/loading_screen.dart
@@ -1,32 +1,20 @@
 import "package:flutter/material.dart";
 
-class LoadingScreen extends StatelessWidget
-{
-  const LoadingScreen(
-    {
-      this.exception,
-      required this.onTryAgain,
-      super.key
-    }
-  );
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({this.exception, required this.onTryAgain, super.key});
 
   final Exception? exception;
   final VoidCallback onTryAgain;
 
   @override
-  Widget build(BuildContext context)
-  {
-    if (exception != null)
-    {
+  Widget build(BuildContext context) {
+    if (exception != null) {
       return Center(
-        child: ElevatedButton(
-          onPressed: onTryAgain,
-          child: const Text("Try Again"),
-        )
-      );
-    }
-    else
-    {
+          child: ElevatedButton(
+        onPressed: onTryAgain,
+        child: const Text("Try Again"),
+      ));
+    } else {
       return const Center(
         child: CircularProgressIndicator(),
       );
