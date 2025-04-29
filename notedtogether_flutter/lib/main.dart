e8e280779994775ac87532b17a98abfc91cd5bc3
Pak_Man926 <davidkamau926@gmail.com>
2025-04-29 22:56:25 +0300

    Auto commit

--- notedtogether_flutter/lib/main.dart
+++ notedtogether_flutter/lib/main.dart
@@ -163,7 +163,7 @@ void _connectionFailed(dynamic exception)
       actions: [
         IconButton(
           onPressed: ,
-           icon: Icon(Icons.))
+           icon: Icon(Icons.re))
       ]
      )
     );
