d72dd0195b5046a7c6a5b2aa8eb866615f7fe993
Pak_Man926 <davidkamau926@gmail.com>
2025-04-29 17:05:42 +0300

    Auto commit

--- notedtogether_flutter/lib/main.dart
+++ notedtogether_flutter/lib/main.dart
@@ -71,9 +71,14 @@ class MyHomePageState extends State<MyHomePage>
   }
 }
 
-class NotesPage extends StatelessWidget {
+class NotesPage extends StatefulWidget {
   const NotesPage({super.key});
 
+  @override
+  State<NotesPage> createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotesPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
